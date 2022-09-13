0da12dadcf20f578cd52d88e2609d6fef8fed3c5
Sebastian Lujan <glujan.recalde@gmail.com>
2022-09-13 13:28:27 -0300

    Add merkle patricia tree validation

--- app/minter/services/merkleTree.js
+++ app/minter/services/merkleTree.js
@@ -10,24 +10,56 @@
 */
 
 const { MerkleTree } = require('merkletreejs')
-const keccak256 = require('keccak256');
+const SHA256 = require('crypto-js/sha256');
+const DB = require('../db.json')
 
-
-const createMerkleTree = () => {
-    const merkleList = [];
-    const leafNodes = merkleList
-
-    
-        const leaves = ['a', 'b', 'c'].map(x => SHA256(x))
-        const tree = new MerkleTree(leaves, SHA256)
-        const root = tree.getRoot().toString('hex')
-        const leaf = SHA256('a')
-        const proof = tree.getProof(leaf)
-        console.log(tree.verify(proof, leaf, root)) // true
-    
+const createList = (DB) => {
+    const merkleList = []
+    DB.map( (nftData, index) => {
+        let data = {}
+        data[index] = nftData.owner_of
+        merkleList.push(data)
+    })
+    console.log(merkleList)
+    return merkleList;
 }
 
-createMerkleTree();
+const addMerkleLeaves = ( merkleList, index ) => {
+    const leaves = merkleList[index][index].map(x => SHA256(x))
+    console.log(leaves)
+    return leaves
+}
+
+const createMerkleTree = (leaves, cb) => {
+    const tree =  new MerkleTree(leaves , cb)
+    return tree;
+}
+
+const getRoot = (tree) => {
+    return tree.getRoot().toString('hex')
+}
+
+const getLeaf = (address) => {return SHA256(address)}
+
+const getProof = (tree, leaf) => {return tree.getProof(leaf)}
+
+const verifyMerkleOwnership = (tree, proof, root, leaf) => {
+    return tree.verify(proof, leaf, root)
+}
 
 
-module.exports = Merkle
+let ownersList = createList(DB);
+
+let leaves = addMerkleLeaves(ownersList , 10)
+const tree = createMerkleTree(leaves, SHA256);
+
+const address = '0x59455ba3d00bb8cbead8fe6db3973210e49d4303';
+
+const proofOwnership = getLeaf(address);
+let proof = getProof(tree, proofOwnership);
+let it_verify = verifyMerkleOwnership( tree, proof, getRoot(tree), proofOwnership);
+
+console.log(it_verify)
+
+
+//module.exports = CreateMerkleTree
