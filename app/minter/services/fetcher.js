679805474b3e0cfd0815defc19b8117afe0ab1fe
Sebastian Lujan <glujan.recalde@gmail.com>
2022-09-13 09:51:35 -0300

    Update on Fetcher, add DB.json

--- app/minter/services/fetcher.js
+++ app/minter/services/fetcher.js
@@ -1,5 +1,5 @@
-const axios = require('axios').default;
-const fs = require('fs');
+const axios = require('axios').default
+const fs = require('fs')
 let schema = require('./schema.js')
 let secrets = require('../secrets.json')
 
@@ -12,47 +12,53 @@ const getnftowners = async (address, tokenId) => {
 		},
 	}
 
-	let request = await axios.get(url, options).then((res) => {
-        return res.data.result
-    })
-    return request;
+	let request = await axios.get(url, options).then(res => {
+		return res.data.result
+	})
+	return request
 }
 
-const populateSchema = (nftData , schema) => {
-    let Schema = schema;
-    let owners = []
+const populateSchema = (nftData, schema) => {
+	let Schema = schema
+	let owners = []
 
-    nftData.map( elem => owners.push(elem.owner_of))
+	nftData.map(elem => owners.push(elem.owner_of))
 
-    //refactor object assign
-    Schema.owner_of = owners
-    Schema.token_id = nftData[0].token_id
-    Schema.token_hash = nftData[0].token_hash
-    Schema.block_number_minted = nftData[0].block_number_minted
-    Schema.block_number = nftData[0].block_number
-    Schema.contract_type = nftData[0].contract_type
-    Schema.token_uri = nftData[0].token_uri
-    Schema.metadata = JSON.parse(nftData[0].metadata)
-    Schema.last_token_uri_sync = nftData[0].last_token_uri_sync
-    Schema.last_metadata_sync = nftData[0].last_metadata_sync
-    return Schema;
+	//refactor object assign
+	Schema.owner_of = owners
+	Schema.token_id = nftData[0].token_id
+	Schema.token_hash = nftData[0].token_hash
+	Schema.block_number_minted = nftData[0].block_number_minted
+	Schema.block_number = nftData[0].block_number
+	Schema.contract_type = nftData[0].contract_type
+	Schema.token_uri = nftData[0].token_uri
+	Schema.metadata = JSON.parse(nftData[0].metadata)
+	Schema.last_token_uri_sync = nftData[0].last_token_uri_sync
+	Schema.last_metadata_sync = nftData[0].last_metadata_sync
+	return Schema
 }
 
 let getSchema = async () => {
-    let address = secrets.ADDRESS
-    
-    let result = []
-    for(let i = 0; i<=10; i++){
-        let nftData = await getnftowners(address, i)
-       
-        await new Promise(r => setTimeout(r, 1000));
-        let Schema = await populateSchema(nftData, schema(address))
-    
-        result.push(Schema)
-        console.log(result)
-    }
-    fs.writeFileSync(__dirname + `/nftData.json`, JSON.stringify(result))
-    return result;
+	let address = secrets.ADDRESS
+
+	let result = []
+	for (let i = 0; i <= 10; i++) {
+		let nftData = await getnftowners(address, i)
+
+		await new Promise(r => setTimeout(r, 1000))
+		let Schema = await populateSchema(nftData, schema(address))
+
+		result.push(Schema)
+		console.log(result)
+	}
+
+	try {
+		fs.writeFileSync(`./bd.json`, JSON.stringify(result))
+        console.log("se ha guardado")
+	} catch (err) {
+		console.error(err)
+	}
+	return result
 }
 
 module.exports = getSchema
--- app/minter/services/merkleTree.js
+++ app/minter/services/merkleTree.js
@@ -17,14 +17,14 @@ const createMerkleTree = () => {
     const merkleList = [];
     const leafNodes = merkleList
 
-    // for (let i = 0; i < 10; i++) {
-    // const leaves = ['a', 'b', 'c'].map(x => SHA256(x))
-    // const tree = new MerkleTree(leaves, SHA256)
-    // const root = tree.getRoot().toString('hex')
-    // const leaf = SHA256('a')
-    // const proof = tree.getProof(leaf)
-    // console.log(tree.verify(proof, leaf, root)) // true
-
+    
+        const leaves = ['a', 'b', 'c'].map(x => SHA256(x))
+        const tree = new MerkleTree(leaves, SHA256)
+        const root = tree.getRoot().toString('hex')
+        const leaf = SHA256('a')
+        const proof = tree.getProof(leaf)
+        console.log(tree.verify(proof, leaf, root)) // true
+    
 }
 
 createMerkleTree();
