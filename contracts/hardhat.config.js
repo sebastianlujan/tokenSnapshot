9928b5267e0f4174a588a4c569317a0afffc138c
Sebastian Lujan <glujan.recalde@gmail.com>
2023-02-16 18:04:52 -0300

    update config-files

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
